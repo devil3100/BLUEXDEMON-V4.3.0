39da49bb234cec4c319a425bc0619cf6645d7d58
devil3100 <whitedragonkmn123@gmail.com>
2025-01-07 00:46:06 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "4915679582752"
 
-global.botNumber = ["2349019529423"]
+global.botNumber = ["4915679582752"]
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
