18bef822ecdf12b912307cb05dbad2fbba77999a
devil3100 <whitedragonkmn123@gmail.com>
2025-01-18 22:36:19 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "4915679582752"
+global.pairingNumber = "491786327165"
 
 
 
